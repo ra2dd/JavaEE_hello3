78beb7a2170729edd03eef036d6740f775ebbc68
ra2dd <radoslawpryw@gmail.com>
2023-05-18 19:18:35 +0200

    create servletContext with mapping and ServletConfig

--- src/main/java/com/example/javaee_hello3/PassingServlet.java
+++ src/main/java/com/example/javaee_hello3/PassingServlet.java
@@ -1,5 +1,7 @@
 package com.example.javaee_hello3;
 
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
 import javax.servlet.http.*;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -16,5 +18,13 @@ public class PassingServlet extends HttpServlet
 
         PrintWriter out = response.getWriter();
         out.println("<a href=hello-servlet> Check if session works <a/>");
+
+        ServletContext context = getServletContext();
+        String color = context.getInitParameter("color");
+        out.println("<p>Context color is " + color + ".");
+
+        ServletConfig config = getServletConfig();
+        String initColor = config.getInitParameter("initColor");
+        out.println("<p>Context init color is " + initColor + ".");
     }
 }
